d7b0af70631923e5e1c02615f022b5ba0b869657
Your Name <deshmukhanjali167@gmail.com>
2024-10-24 10:15:52 +0530

    update

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { FirstpageComponent } from './component/firstpage/firstpage.component';
+import { HomepageComponent } from './component/homepage/homepage.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'',
+        redirectTo:"home",
+
+        pathMatch:'full'
+    }
+    ,{
+        path:"home",
+        component:HomepageComponent
+    }
+    ,{
+        path:"first",
+        component:FirstpageComponent
+    }
+];
