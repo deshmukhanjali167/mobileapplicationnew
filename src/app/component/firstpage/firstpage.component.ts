bfb3a0c0c5a7b722c96beeead3340c9718d53a43
Your Name <deshmukhanjali167@gmail.com>
2024-10-24 10:24:24 +0530

    chnages

--- src/app/component/firstpage/firstpage.component.ts
+++ src/app/component/firstpage/firstpage.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-firstpage',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './firstpage.component.html',
   styleUrl: './firstpage.component.css'
 })
